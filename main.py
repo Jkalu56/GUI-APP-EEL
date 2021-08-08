3689b49c4e427ec0955afd0acdfd01208cc3ea07
First Name Last Name <75053826+Jkalu56@users.noreply.github.com>
2021-08-08 03:24:18 +0000

    Cleaned up process! Looks much better now! Keep it!

--- main.py
+++ main.py
@@ -1,54 +1,23 @@
 import eel
 
-# Okay, now I just need a data dictionary imported
-# Once I have that, I'll know how to proceed.
-# This program will just update once per second.
-
-
 eel.init('web')
 
 
-def retrieve_data(optionalvalue=None):
-    with open("sample.csv", 'r') as file:
-        rd = file.readlines()
-        rd2 = []
-    for line in rd:
-        line = line.replace('\n', '')
-        line = line.split(',')
-        rd2.append(line)
-    keys = rd2[0]
-    values = rd2[-1]
-    return dict(zip(keys, values)) if not optionalvalue else dict(zip(keys, values))[optionalvalue]
-
-
+#The only function main.js calls now ;)
 @eel.expose
-def get_latitude():
-    latitude = retrieve_data("Latitude")
-    return latitude
+def retrieve_data():
+	with open("sample.csv", 'r') as file:
+		rd = file.readlines()
+	if len(rd[-1]) > 1: values = rd[-1].replace('\n', '').split(",")
+	else: values = rd[-2].replace('\n', '').split(",")
+	keys = rd[0][:-1].split(",")
+	return dict(zip(keys, values))
 
-@eel.expose
-def get_longitude():
-    longitude = retrieve_data("Longitude")
-    return longitude
 
-@eel.expose
-def get_altitude():
-    altitude = retrieve_data("Altitude")
-    return altitude
-
-@eel.expose
-def get_ext_temperature():
-    temp = retrieve_data("External_Temperature")
-    return temp
-
-@eel.expose
-def get_int_temperature():
-    temp = retrieve_data("Internal_Temperature")
-    return temp
-
+#This code is for debugging purposes
 @eel.expose
 def print_stuff(message):
-  print(message)
+	print(message)
 
 
 eel.start('index.html', host="0.0.0.0", port=8080)
--- web/js/main.js
+++ web/js/main.js
@@ -1,70 +1,62 @@
 eel.expose(prompt_alerts);
 function prompt_alerts(description) {
-  alert(description);
+    alert(description);
 }
 
-var map, Marker, Data
-async function createMap(){
-  let latitude = await eel.get_latitude()();
-  let longitude = await eel.get_longitude()();
-  var mapOptions = {
-  center: [latitude, longitude],
-  zoom: 40,
-  watch: true,
-  }
+var map, Marker;
 
-  // Creating a map object
-  map = new L.map('map', mapOptions);
+async function createMap() {
+    pythonData = await eel.retrieve_data()();
+    let latitude = pythonData["Latitude"];
+    let longitude = pythonData["Longitude"];
+    var mapOptions = {
+        center: [latitude, longitude],
+        zoom: 40,
+        watch: true,
+    }
 
-  // Creating a Layer object
-  var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    map = new L.map('map', mapOptions);
 
-  // Adding layer to the map
-  map.addLayer(layer); 
+    var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
-  var myIcon = L.divIcon({className: 'my-div-icon'});
-  Marker = L.marker([latitude, longitude], {icon:myIcon}).addTo(map)
-    .bindPopup('Payload Position')
-    .openPopup();
+    map.addLayer(layer);
+
+    var myIcon = L.divIcon({ className: 'my-div-icon' });
+    Marker = L.marker([latitude, longitude], { icon: myIcon }).addTo(map)
+        .bindPopup('Payload Position')
+        .openPopup();
 };
 
-async function update_map(){
-  lat = await eel.get_latitude()();
-  lng = await eel.get_longitude()();
-  Marker.setLatLng([lat, lng]).update();
-  map.panTo([lat, lng]);
+async function update() {
+    pythonData = await eel.retrieve_data()();
+    lat = pythonData["Latitude"];
+    lng = pythonData["Longitude"];
+    Marker.setLatLng([lat, lng]).update();
+    map.panTo([lat, lng]);
+
+    for (var key in pythonData) {
+        var value = pythonData[key];
+        document.getElementById(key).innerHTML = key + ': ' + value;
+    }
 }
+
+async function get_time() {
+    var clock = document.getElementById("clock");
+    var time = new Date();
+    var now = time.toLocaleString('en-US', { hour12: true })
+    clock.innerHTML = now + " EST";
+}
+
+
+/* 
+Let's get everything going!
+*/
 
 createMap();
 
+/*
+the interval for updating data is set to update every 1 seconds
+*/
 
-function timed_functions(){
-  get_time();
-  retrieve_data();
-  //eel.print_stuff(Data.longitude);
-  for (var key in Data){
-    var value = Data[key];
-    document.getElementById(key).innerHTML = key+': '+value;
-  }
-}
-
-async function get_time(){
-  var clock = document.getElementById("clock");
-  var time = new Date();
-  var now = time.toLocaleString('en-US', {hour12: true })
-  clock.innerHTML = now + " EST";
-}
-
-
-async function retrieve_data(){
-  Data = {
-    "altitude": await eel.get_altitude()(),
-    "latitude": await eel.get_latitude()(),
-    "longitude": await eel.get_longitude()(),
-    "ext_temperature": await eel.get_ext_temperature()(),
-    "int_temperature": await eel.get_int_temperature()(),
-  }
-}
-
-setInterval(timed_functions, 1000);
-setInterval(update_map, 1000);
+setInterval(get_time, 1000);
+setInterval(update, 1000);
